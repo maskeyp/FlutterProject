eae0f01824fa23d15fac37cc9e30387e58b95a01
maskeyp <maskeypawan123@gmail.com>
2021-12-02 18:59:32 +0545

    day2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  int days = 30;
+  String name = "Maskey";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome to Ekrishi"),
+      ),
+        
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
