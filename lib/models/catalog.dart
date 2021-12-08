daac8e4f6a932df2a25e20c2540f5b4055bc2247
maskeyp <maskeypawan123@gmail.com>
2021-12-08 23:27:41 +0545

    day10 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,3 +1,17 @@
+class CatalogModel {
+  static final items = [
+Item(
+      id: "maskey001",
+      name: "iPhone 12 Pro",
+      desc: "Apple iphone 12th generation",
+      price: 1299,
+      color: "#33505a",
+      image:
+          "https://www.apple.com/newsroom/images/product/availability/Apple_iphone12mini-iphone12max-homepodmini-availability_iphone12promax-us_110520_inline.jpg.large.jpg")
+
+  ];
+}
+
 class Item {
   final String id;
   final String name;
@@ -6,14 +20,9 @@ class Item {
   final String color;
   final String image;
 
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
 }
 
-final products= [
-  id: "maskey001",
-  name: "iPhone 12 Pro",
-  desc: "Apple iphone 12th generation",
-  price: 1299,
-  color: "#33505a",
-  image: "https://www.apple.com/newsroom/images/product/availability/Apple_iphone12mini-iphone12max-homepodmini-availability_iphone12promax-us_110520_inline.jpg.large.jpg"
-]
+final products = [
+  
+];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:learning_flutter/models/catalog.dart';
 import 'package:learning_flutter/widgets/drawer.dart';
+import 'package:learning_flutter/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   int days = 30;
@@ -7,19 +9,22 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          " Ekrishi",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-      ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of flutter by $name"),
+          title: Text(
+        "Hamro E-Krishi",
+        style: TextStyle(color: Colors.black),
+      )),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: CatalogModel.items[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
