a47736a6d26b334c1be3a93607afd51e535eb1aa
maskeyp <maskeypawan123@gmail.com>
2021-12-08 20:55:38 +0545

    day9 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,19 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final products= [
+  id: "maskey001",
+  name: "iPhone 12 Pro",
+  desc: "Apple iphone 12th generation",
+  price: 1299,
+  color: "#33505a",
+  image: "https://www.apple.com/newsroom/images/product/availability/Apple_iphone12mini-iphone12max-homepodmini-availability_iphone12promax-us_110520_inline.jpg.large.jpg"
+]
